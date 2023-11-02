b3f8024bd15fb2368ca4a639fc969bbe9491e6c5
TheSaviourEking <saviour.eking@outlook.com>
2023-11-02 17:47:59 +0100

    SOLVED: fix Binary-search tree Part 1 solution -> insert

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -11,10 +11,46 @@ class BinarySearchTree {
 
   constructor() {
     // Your code here
+    this.root = null;
   }
 
-  insert(val, currentNode=this.root) {
+  insert(val, currentNode = this.root) {
     // Your code here
+    let node = new TreeNode(val);
+    if (!this.root) {
+      this.root = node;
+    } else {
+      let current = this.root;
+      // if (node.val < this.root.val) {
+      //   // this.root.left = node;
+      //   while (current.val ){}
+      // } else {
+      //   if (node.val > this.root.val) {
+      //     this.root.right = node;
+      //   }
+      // }
+
+      while (current) {
+        // console.log(current, 'current', node.val < current.val);
+        if (node.val < current.val) {
+          if (current.left) {
+            current = current.left;
+          } else {
+            current.left = node;
+            break;
+          }
+        } else if (node.val > current.val) {
+          if (current.right) {
+            current = current.right;
+          } else {
+            current.right = node;
+            break;
+          }
+        }
+      }
+
+      // console.log('current AFTER', current)
+    }
   }
 
   search(val) {
@@ -36,7 +72,7 @@ class BinarySearchTree {
     // Your code here
   }
 
-    // Breadth First Traversal - Iterative
+  // Breadth First Traversal - Iterative
   breadthFirstTraversal() {
     // your code here
   }
@@ -44,7 +80,7 @@ class BinarySearchTree {
   // Depth First Traversal - Iterative
   depthFirstTraversal() {
     // your code here
-}
+  }
 }
 
 module.exports = { BinarySearchTree, TreeNode };
